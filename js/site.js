82172939936aabc9af881620b06722a6cad729aa
SJohnson <simonbjohnson@gmail.com>
2018-08-30 19:35:37 +0100

    project template

---
+++ js/site.js
@@ -0,0 +1,43 @@
+function generateDash(data,geom){
+
+}
+
+function hxlProxyToJSON(input,headers){
+    var output = [];
+    var keys=[]
+    input.forEach(function(e,i){
+        if(i==0){
+            e.forEach(function(e2,i2){
+                var parts = e2.split('+');
+                var key = parts[0]
+                if(parts.length>1){
+                    var atts = parts.splice(1,parts.length);
+                    atts.sort();                    
+                    atts.forEach(function(att){
+                        key +='+'+att
+                    });
+                }
+                keys.push(key);
+            });
+        } else {
+            var row = {};
+            e.forEach(function(e2,i2){
+                row[keys[i2]] = e2;
+            });
+            output.push(row);
+        }
+    });
+    return output;
+}
+
+var dataCall = $.ajax({ 
+    type: 'GET', 
+    url: '',
+    dataType: 'json',
+});
+
+$.when(dataCall).then(function(dataArgs){
+    var data = dataArgs;
+    data = hxlProxyToJSON(data);
+    generateDash(data);
+});
